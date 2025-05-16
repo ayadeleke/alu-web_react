e6cc6940c4258a333754efb0df01e1cc4ef9fda9
@a.adeleke <a.adeleke@alustudent.com>
2025-05-16 11:16:33 -0500

    task 1_dir

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,4 +15,18 @@ const teacher3: Teacher = {
     contract: false,
 };
 
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
 console.log(teacher3);
