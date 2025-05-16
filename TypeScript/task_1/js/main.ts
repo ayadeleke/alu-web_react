3b942b28de14506f9e8cd8a8170dbab2ff960822
@a.adeleke <a.adeleke@alustudent.com>
2025-05-16 11:18:34 -0500

    task 1_all

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -27,6 +27,38 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+export interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string) {
+    return `${firstName.slice(0, 1)}. ${lastName}`;
+}
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
 console.log(director1);
 
 console.log(teacher3);
