bbae069049d6aaa12d4f7caf5473693ad2b8dc49
@a.adeleke <a.adeleke@alustudent.com>
2025-05-20 00:50:44 -0500

    task_5

--- React_component/task_0/dashboard/config/setupTests.js
+++ React_component/task_0/dashboard/config/setupTests.js
@@ -1,5 +1,5 @@
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-17';
-import 'regenerator-runtime/runtime'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import 'regenerator-runtime'
 
 Enzyme.configure({ adapter: new Adapter() });
