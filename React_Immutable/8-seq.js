a5692b6b602b79fc32f6e371e511f14b2cf7c466
@a.adeleke <a.adeleke@alustudent.com>
2025-05-31 00:48:49 +0200

    task 8

--- React_Immutable/8-seq.js
+++ React_Immutable/8-seq.js
@@ -1,12 +1,12 @@
 import { Seq } from 'immutable';
 
 export function printBestStudents(object) {
-  const bestStudents = Seq(object)
-    .filter(student => student.score >= 70)
-    .map(student => ({
-      score: student.score,
-      firstName: `${student.firstName[0].toUpperCase()}${student.firstName.slice(1)}`,
-      lastName: `${student.lastName[0].toUpperCase()}${student.lastName.slice(1)}`,
-    }));
-  return bestStudents.toObject();
+    const bestStudents = Seq(object)
+        .filter(student => student.score >= 70)
+        .map(student => ({
+        score: student.score,
+        firstName: `${student.firstName[0].toUpperCase()}${student.firstName.slice(1)}`,
+        lastName: `${student.lastName[0].toUpperCase()}${student.lastName.slice(1)}`,
+        }));
+    return bestStudents.toObject();
 }
